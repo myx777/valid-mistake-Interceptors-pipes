fe10dc405d3bf49477d6796e104d0039b2ac0254
myx777 <anton.kruglov89@gmail.com>
2024-10-19 22:56:40 +0300

    completed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './http.exception.filter';
 
+@UseFilters(HttpExceptionFilter)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    // для тестирования
+    // throw new HttpException('Oops', 404);
+
     return this.appService.getHello();
   }
 }
